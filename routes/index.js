53227ddef461f427fbd54d3babae0a6dc4a25900
Manish Verma <manish.verma@biz2credit.org>
2021-05-29 18:03:07 +0530

    std3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is dev branch' });
+  res.render('index', { title: 'This is master branch' });
 });
 
 module.exports = router;
